5791f8a6865c45747fa6c2b300ce92f4638344e1
CNK_신민정 <smj@clickntouch.co.kr>
2022-10-18 11:07:43 +0900

    first project setting

---
+++ src/main/java/com/example/springgradleex/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.springgradleex;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringGradleExApplication.class);
+    }
+
+}
---
+++ src/test/java/com/example/springgradleex/SpringGradleExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgradleex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGradleExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
